95fce1f96823e5a115aa3b30c33dcafdecf20dca
Guillermo Coba Montoya <guillermo.coba@digitalfemsa.com>
2023-06-09 11:24:16 -0600

    test postman

---
+++ RedocPro/Controllers/ValuesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RedocPro.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        [ProducesResponseType(200)]
+        public IActionResult Health()
+        {
+            return this.Ok("0.1.5");
+        }
+    }
+}
