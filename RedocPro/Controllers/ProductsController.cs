17caae233772608b7827cdd424f2f39a3bd1facf
Guillermo Coba Montoya <guillermo.coba@digitalfemsa.com>
2023-06-11 22:44:56 -0600

    test actualizar postman

---
+++ RedocPro/Controllers/ProductsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RedocPro.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet("nombre/{id}")]
+        public string LeerNombre(int id)
+        {
+            return id switch
+            {
+                1 => "Net",
+                2 => "mentor",
+                _ => throw new System.NotImplementedException()
+            };
+        }
+    }
+}
