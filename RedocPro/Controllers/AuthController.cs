7cce26cef72930fef561d8f382079ed84a32683a
Guillermo Coba Montoya <guillermo.coba@digitalfemsa.com>
2023-06-13 15:47:37 -0600

    add descriptions

--- RedocPro/Controllers/AuthController.cs
+++ RedocPro/Controllers/AuthController.cs
@@ -59,41 +59,29 @@ namespace RedocPro.Controllers
             return this.Ok();
         }
 
-        [HttpPost("ChangeUserPassword")]
-        [SwaggerOperation(
-            Summary = "Change User Password",
-            Description = "This endpoint will change the password of the user.",
-            OperationId = "ChangeUserPassword")]
-        [ProducesResponseType(200, Type = typeof(UserChangeResponse))]
+        [HttpPost]
+        [SwaggerOperation(OperationId = nameof(ChangeUserPassword), Description = EndpointsDescriptions.ChangeUserPassword)]
+        [SwaggerResponse(200, "Response", type: typeof(UserChangeResponse))]
+        [SwaggerResponse(404, "SPCI-404: User not found", type: typeof(Error))]
         public async Task<IActionResult> ChangeUserPassword([FromBody] UpdatePasswordRequest request)
         {
             return this.Ok();
         }
-        /// <summary>
-        /// Change User Property
-        /// </summary>
-        /// <remarks>This endpoint will change the property of the user.</remarks>
-        /// <response code="200">Returns a 200 if the user property change.</response>
-        /// <response code="500">Unhandled error, validate the error log.</response>
-        [HttpPost("ChangeUserProperty")]
-       
-        [ProducesResponseType(200, Type = typeof(UserChangeResponse))]
+
+
+        [HttpPost]
+        [SwaggerOperation(OperationId = nameof(ChangeUserProperty), Description = EndpointsDescriptions.ChangeUserProperty)]
+        [SwaggerResponse(200, "Returns a 200 if the user property change.")]
+        [SwaggerResponse(404, "SPCI-404: User not found", type: typeof(Error))]
         public async Task<IActionResult> ChangeUserProperty([FromBody] UpdateUserRequest request)
         {
             return this.Ok();
         }
-        /// <summary>
-        /// User Validation
-        /// </summary>
-        /// <remarks>
-        /// This endpoint will validate if the user has any of the following states: 
-        /// <ul><li>NotRegistered = 0</li><li>PreRegistered = 1</li><li>Registered = 2</li><li>OTPRequired = 3</li>
-        /// <li>SoftBlock = 4</li><li>HardBlock = 5</li><li>CancelledAccount = 8</li><li>FirstLoginOxxo = 20</li></ul>
-        /// </remarks>
-        /// <response code="200">Returns a status of the user.</response>
-        /// <response code="500">Unhandled error, validate the error log.</response>
-        [HttpGet("UserValidation")]
-        [ProducesResponseType(200, Type = typeof(UserValidationResponse))]
+
+        [HttpGet]
+        [SwaggerOperation(OperationId = nameof(UserValidation), Description = EndpointsDescriptions.UserValidation)]
+        [SwaggerResponse(200, "Response", type: typeof(UserValidationResponse))]
+        [SwaggerResponse(404, "SPCI-404: User not found", type: typeof(Error))]
         public async Task<IActionResult> UserValidation([FromQuery] UserValidationRequest request)
         {
             return this.Ok();
--- RedocPro/Descriptions/EndpointsDescriptions.cs
+++ RedocPro/Descriptions/EndpointsDescriptions.cs
@@ -5,5 +5,12 @@
 		public const string GetTransversalUserDataDescription = @"Some endpoint description goes here.";
 		public const string RevokeRefreshTokenDescription = @"Some endpoint description goes here.";
 		public const string GetUserProfileDescription = @"Some endpoint description goes here.";
-	}
+		public const string UserValidation = @"This endpoint will validate if the user has any of the following states: 
+											   <ul><li>NotRegistered = 0</li><li>PreRegistered = 1</li><li>Registered = 2</li>
+											   <li>OTPRequired = 3</li><li>SoftBlock = 4</li><li>HardBlock = 5</li>
+											   <li>CancelledAccount = 8</li><li>FirstLoginOxxo = 20</li></ul>";
+		public const string ChangeUserProperty = @"This endpoint will change the property of the user.";
+		public const string ChangeUserPassword = @"This endpoint will change the password of the user.";
+
+    }
 }
