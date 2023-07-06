55d7cafa3ed7c435d194d3274bcdb95967bdf3af
Guillermo Coba <guillermo.coba@digitalfemsa.com>
2023-07-06 15:26:53 -0600

    [SPPP-666]: test

--- RedocPro/Descriptions/ControllersDescriptions.cs
+++ RedocPro/Descriptions/ControllersDescriptions.cs
@@ -5,6 +5,6 @@
         public const string AuthController = @"Auth controller description goes here.";
         public const string HealthController = @"Health controller description goes here.";
         public const string NethoneController = @"Nethone controller description goes here.";
-        public const string UserController = @"User controller description goes here.";
+        public const string UserController = @"User controller description goes here";
     }
 }
