9666ac0f2ba9769152518c408bf3368ea55a55db
Ricardo Koh Pat <ricardo.koh@digitalfemsa.com>
2023-07-12 16:07:19 -0600

    [SPPP-624]: Ajustes al .sh para evitar conflictos al correr N veces

--- run_app.sh
+++ run_app.sh
@@ -3,18 +3,25 @@
 # Set environment variables copied from container
 source /app/set_env.sh;
 
-# Run your dotnet console app
-dotnet run --no-restore --no-build --project /app/RedocPro/RedocPro.csproj --configuration Debug & sleep 20 && \
-    echo "Run the API" && \
-    PID=$(pgrep -f "dotnet run --no-restore --no-build --project /app/RedocPro/RedocPro.csproj --configuration Debug") && \
-    files=$(find /app/swagger_files -name "swaggerv*.json") && \
-    for file in $files; do \
-      version=$(basename "$file" .json | sed 's/version_//') && \
-      echo "Building and Redoc documentation $version" && \
-      redoc-cli bundle "$file" --output "/app/docs/$version/index.html" ; \
-      echo "Building the Postman collection version $version" && \
-      openapi2postmanv2 -s "$file" -o "/app/docs/$version/postman_collection.json"  && \
-        (Get-Content -Raw -Path "/app/docs/$version/postman_collection.json") | ForEach-Object {  $_ -replace '^', '{"collection":' } | Set-Content -Path "/app/Docs/$version/postman_collection.json"  && \
-      Add-Content -Path "/app/docs/$version/postman_collection.json" -Value "}"; \ 
-    done && \
-    kill "$PID"
+echo "#############################################################"
+echo "Running the API"
+dotnet run --no-restore --no-build --project /app/RedocPro/RedocPro.csproj --configuration Debug &
+sleep 20
+PID=$(pgrep -f "dotnet run --no-restore --no-build --project /app/RedocPro/RedocPro.csproj --configuration Debug")
+files=$(find /app/swagger_files -name "swaggerv*.json")
+for file in $files; do
+  version=$(basename "$file" .json | sed 's/version_//')
+  echo "#############################################################"
+  echo "Building and Redoc documentation $version"
+  redoc-cli bundle "$file" --output "/app/docs/$version/index.html"
+  echo "#############################################################"
+  echo "Building the Postman collection version $version "
+  openapi2postmanv2 -s "$file" -o "/app/docs/$version/postman_collection.json" 
+  content=$(cat "/app/docs/$version/postman_collection.json")
+  content=$(echo "$content" | sed '1s/^/{"collection":/')
+  content=$(echo "$content" | sed '$s/$/}/')
+  echo "$content" > "/app/docs/$version/postman_collection.json"
+done
+echo "#############################################################"
+echo "Stopping the API"
+kill "$PID"
